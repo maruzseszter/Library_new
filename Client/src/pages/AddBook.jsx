5cf5968cf27f3a1fa023d30a34caceef843d02ee
maruzseszter <eszter.maruzs@gmail.com>
2024-02-07 22:26:58 +0100

    könyv hozzáadása funkció (mindenki)

--- Client/src/pages/AddBook.jsx
+++ Client/src/pages/AddBook.jsx
@@ -1,6 +1,43 @@
+import { FormRow } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { BOOK_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
 
 const AddBook = () => {
-  return <h1>AddBook</h1>;
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting'
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>Könyv hozzáadása</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='title' labelText='cím' />
+          <FormRow type='text' name='author' labelText='szerző' />
+          <FormRow type='number' name='ISBN' />
+          <FormRow type='text' name='publisher' labelText='kiadó' />
+          <FormRow type='number' name='year' labelText='év' />
+          {/* <FormRowSelect
+            labelText='kategória'
+            name='bookType'
+            list={Object.values(BOOK_TYPE)}
+          /> */}
+          <FormRow type='number' name='quantity' labelText='darabszám' />
+          <button
+            type='submit'
+            className='btn btn-block form-btn '
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'hozzáad'}
+          </button>
+
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default AddBook;
