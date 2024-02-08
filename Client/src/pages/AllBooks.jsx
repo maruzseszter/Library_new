940194ccd2e328447435f2688fe43b24d5c12427
maruzseszter <eszter.maruzs@gmail.com>
2024-02-08 19:46:04 +0100

    SearchContainer, BooksContainer hozzáadása

--- Client/src/pages/AllBooks.jsx
+++ Client/src/pages/AllBooks.jsx
@@ -1,6 +1,27 @@
+import { toast } from 'react-toastify';
+import { BooksContainer, SearchContainer } from '../components';
+import customFetch from '../utils/customFetch';
+import { useLoaderData } from 'react-router-dom';
+import { useContext, createContext } from 'react';
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/books');
+    return { data }
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const AllBooks = () => {
-    return <h1>AllBooks</h1>;
-  };
-  
-  export default AllBooks;
+  const { data } = useLoaderData();
+  return (
+    <>
+      <SearchContainer />
+      <BooksContainer />
+    </>
+  )
+};
+
+export default AllBooks;
