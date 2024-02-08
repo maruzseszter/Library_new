6a7ebb0db5a044de22be8f1ffb533bd480d13d0d
maruzseszter <eszter.maruzs@gmail.com>
2024-02-08 19:52:03 +0100

    BooksContainer, SearchContainer hozzáadása

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -4,4 +4,7 @@ export {default as BigSidebar } from './BigSidebar';
 export {default as SmallSidebar } from './SmallSidebar';
 export {default as Navbar } from './Navbar';
 export {default as FormRowSelect } from './FormRowSelect';
+export {default as BooksContainer } from './BooksContainer';
+export {default as SearchContainer } from './SearchContainer';
+
 
