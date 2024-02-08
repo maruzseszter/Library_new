ec98aaf8ae3bd81e19eb2a81bbe3cb3726a102f3
maruzseszter <eszter.maruzs@gmail.com>
2024-02-08 19:48:56 +0100

    Létrehozva

---
+++ Client/src/components/BooksContainer.jsx
@@ -0,0 +1,33 @@
+// import Book from './Book';
+// import Wrapper from '../assets/wrappers/BooksContainer';
+// import { useAllBooksContext } from '../pages/AllBooks';
+
+const BooksContainer = () => {
+  return <h2>BooksContainer</h2>;
+};
+
+export default BooksContainer;
+
+  // const { data } = useAllBooksContext();
+  // const { books } = data;
+  // if (books.length === 0) {
+    // return 
+    // (
+    //   <Wrapper>
+        // <h2>BooksContainer</h2>;
+      // </Wrapper>
+//    );
+  // }
+
+  // return (
+  //   <Wrapper>
+  //     <div className='books'>
+  //       {books.map((book) => {
+  //         return <Book key={book._id} {...book} />;
+  //       })}
+  //     </div>
+  //   </Wrapper>
+  // );
+// };
+
+// export default BooksContainer;
