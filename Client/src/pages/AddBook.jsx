67526bdc7d2ca5b665ec0434d0863fd5d5eb28f4
maruzseszter <eszter.maruzs@gmail.com>
2024-02-08 17:49:03 +0100

    Könyv hozzáadása után átirányít az AllBooks oldalra

--- Client/src/pages/AddBook.jsx
+++ Client/src/pages/AddBook.jsx
@@ -6,6 +6,20 @@ import { Form, useNavigation, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
 
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+   try {
+     await customFetch.post('/books', data);
+     toast.success('Könyv sikeresen hozzáadva');
+    return redirect('all-books');
+   } catch (error) {
+     toast.error(error?.response?.data?.msg);
+     return error;
+   }
+};
+
+
 const AddBook = () => {
   const { user } = useOutletContext();
   const navigation = useNavigation();
