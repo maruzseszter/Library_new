107a7f9afe535269d673411fb4f61636fdf25a26
maruzseszter <eszter.maruzs@gmail.com>
2024-02-08 21:55:26 +0100

    Book komponens integrálása

--- Client/src/components/BooksContainer.jsx
+++ Client/src/components/BooksContainer.jsx
@@ -1,33 +1,43 @@
-// import Book from './Book';
-// import Wrapper from '../assets/wrappers/BooksContainer';
-// import { useAllBooksContext } from '../pages/AllBooks';
+import Book from './Book';
+import Wrapper from '../assets/wrappers/BooksContainer';
+import { useAllBooksContext } from '../pages/AllBooks';
 
 const BooksContainer = () => {
-  return <h2>BooksContainer</h2>;
+  const { data } = useAllBooksContext();
+  const { books } = data;
+  if (books.length === 0) {
+    return (
+      <Wrapper>
+        <h2>Nincsenek megjeleníthető könyvek....</h2>;
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className='books'>
+        {books.map((book) => {
+          return <Book key={book._id} {...book} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+
 
 export default BooksContainer;
 
-  // const { data } = useAllBooksContext();
-  // const { books } = data;
-  // if (books.length === 0) {
-    // return 
-    // (
-    //   <Wrapper>
-        // <h2>BooksContainer</h2>;
-      // </Wrapper>
-//    );
-  // }
 
-  // return (
-  //   <Wrapper>
-  //     <div className='books'>
-  //       {books.map((book) => {
-  //         return <Book key={book._id} {...book} />;
-  //       })}
-  //     </div>
-  //   </Wrapper>
-  // );
+
+
+// return (
+//   <Wrapper>
+//     <div className='books'>
+//       {books.map((book) => {
+//         return <Book key={book._id} {...book} />;
+//       })}
+//     </div>
+//   </Wrapper>
+// );
 // };
 
 // export default BooksContainer;
