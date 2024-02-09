873b3a16ea5907d1a5d8966689c9192eb1de1c1e
maruzseszter <eszter.maruzs@gmail.com>
2024-02-09 13:58:25 +0100

    Törlés funkció működik

--- Client/src/pages/DeleteBook.jsx
+++ Client/src/pages/DeleteBook.jsx
@@ -1,6 +1,13 @@
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+import { redirect } from 'react-router-dom';
 
-const DeleteBook = () => {
-    return <h1>DeleteBook</h1>;
-  };
-  
-  export default DeleteBook;
+export const action = async ({ params }) => {
+  try {
+    await customFetch.delete(`/books/${params.id}`);
+    toast.success('könyv sikeresen törölve');
+  } catch (error) {
+    toast.error(error.response.data.msg);
+  }
+  return redirect('/dashboard/all-books');
+};
